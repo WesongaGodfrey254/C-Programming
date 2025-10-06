3689332c39a71abd792265cf441b5892907de057
WesongaGodfrey254 <wesongagodfrey60@gmail.com>
2025-10-06 12:16:50 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,19 @@
+/*
+Name:Godfrey wesonga
+RegNo:PA106/G/28754/25
+Description:c program that uses a for loop to print all the numbers from 100down to 50
+			in descendind order.
+*/
+
+#include <stdio.h>
+
+//main function
+int main(){
+	int i=0;
+	
+		for(i=100;i>=50;i--){
+			printf("%d\n",i);
+		}
+	
+	return 0;
+}			
